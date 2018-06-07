df7ac43873c86576ce49aaa6ddaf40e1dbeaeaeb
lockonmaram <maramstratos@gmail.com>
2018-06-07 16:54:02 +0700

    countdown done

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const { timer } = require("./controllers");
+const controllers = require("./controllers");
 
 const main = () => {
+  let timer = controllers[0];
+  let time = controllers[1];
   const seconds = parseInt(process.argv[2]) || 10;
   timer(seconds);
 };
