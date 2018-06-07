db1f35cd2b0a68cb542bb025217ca3dbe65dcba9
vinesto <oqvinesto@gmail.com>
2018-06-07 16:32:52 +0700

    first commit

--- controllers.js
+++ controllers.js
@@ -1,9 +1,31 @@
-const { displayCountdown } = require("./views");
+const displayCountdown = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  displayCountdown(seconds)
+  // var myVar = setInterval(timer,1000)
+
+  // function time(){
+  //   if(seconds === 0){
+  //     clearInterval(0)
+  //   }else{
+  //     seconds--;
+  //     displayCountdown(seconds)
+  //   }
+  // }
+
+  var time = setInterval(function(){
+    if(seconds === 0){
+      clearInterval(0)
+    }else{
+      seconds--;
+      displayCountdown(seconds);
+    }
+  },1000)
+
+
 };
 
 module.exports = {
-  timer
+  timer: timer
 };
--- views.js
+++ views.js
@@ -5,7 +5,12 @@ const chalk = require("chalk");
 const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
+  console.log(chalk.cyan(figlet.textSync(`${seconds}`)));
+  // console.log()
   // Your code here...
 };
 
-module.exports = { displayCountdown };
+module.exports = displayCountdown;
+// console.log(chalk.cyan(figlet.textSync("Countdown App")));
+// displayCountdown()
+
