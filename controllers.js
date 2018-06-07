0dc148d09ee6f762bb47fed9e81308853699cdc5
ronifajrian <ronifajrian@gmail.com>
2018-06-07 16:45:27 +0700

    change format time

--- controllers.js
+++ controllers.js
@@ -4,13 +4,35 @@ const timer = seconds => {
   // Your code here...
   var counting = setInterval(function () {
     // console.log(seconds);
-    displayCountdown(seconds)
+    
+    var formatTime = convert(seconds)
+    var displayTime = formatTime
+
+    displayCountdown(displayTime)
     seconds--
     if (seconds < 0) {
       clearInterval(counting)
     }
   }, 1000)  
 };
+
+function convert(seconds) {
+  var jam = Math.floor(seconds / 60)
+  var menit = seconds % 60
+
+  jam = jam.toString()
+  menit = menit.toString()
+
+  if (jam.length == 1 || jam == '0') {
+    jam = `0${jam}`
+  }else
+  if (menit.length == 1 || menit == '0') {
+    menit = `0${menit}`
+  }
+  return `${jam} : ${menit}`
+  
+  
+}
 
 
 
