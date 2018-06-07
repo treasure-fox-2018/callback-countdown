29967e88b4be6da0d0ae3830d56beae7abe0f699
“Ollas2204” <fitrisakinah17@gmail.com>
2018-06-07 16:05:58 +0700

    callback-countdown

--- controllers.js
+++ controllers.js
@@ -1,7 +1,16 @@
-const { displayCountdown } = require("./views");
+const {displayCountdown} = require('./views');
 
 const timer = seconds => {
-  // Your code here...
+  setInterval(function() {
+    if (seconds >= 0) {
+      displayCountdown(seconds);
+      seconds = seconds - 1;
+    } else {
+      displayCountdown('BOOM');
+      clearInterval(this);
+    }
+  }, 1000);
+
 };
 
 module.exports = {
--- views.js
+++ views.js
@@ -1,11 +1,17 @@
-const clear = require("clear");
-const figlet = require("figlet");
-const chalk = require("chalk");
+const clear = require('clear');
+const figlet = require('figlet');
+const chalk = require('chalk');
 
 const displayCountdown = seconds => {
   clear();
-  console.log(chalk.cyan(figlet.textSync("Countdown App")));
-  // Your code here...
+  console.log(chalk.cyan(figlet.textSync('Countdown App')));
+  if (typeof seconds === 'string') {
+  	console.log(chalk.red(figlet.textSync(seconds, 'Blocks')));
+  } else if (seconds < 10) {
+    console.log(chalk.red(figlet.textSync(`00:0${seconds}`, 'Ghoulish')));
+  } else {
+    console.log(chalk.red(figlet.textSync(`00:${seconds}`, 'Ghoulish')));
+  }
 };
 
-module.exports = { displayCountdown };
+module.exports = {displayCountdown};
