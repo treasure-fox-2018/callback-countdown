4eb2a54d8300d3140ad6449b06567fb0cb404d92
handipriyono <handimacbook@gmail.com>
2018-06-07 16:18:51 +0700

    add cntdown

--- controllers.js
+++ controllers.js
@@ -1,7 +1,21 @@
-const { displayCountdown } = require("./views");
+const {
+  displayCountdown
+} = require("./views")
 
 const timer = seconds => {
   // Your code here...
+  let interval = setInterval(function () {
+    seconds -= 1;
+    displayCountdown(seconds);
+
+    if (seconds === 0) {
+      clearInterval(interval);
+    }
+
+  }, 1000)
+
+
+
 };
 
 module.exports = {
