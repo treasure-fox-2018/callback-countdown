db09cdb44d2b26af1ab05cef93e5f82cc38ea822
sumarsanaadi <sumarsana.adi@gmail.com>
2018-06-07 16:07:23 +0700

    callback countdown sumarsana-adi

--- controllers.js
+++ controllers.js
@@ -1,8 +1,28 @@
 const { displayCountdown } = require("./views");
 
 const timer = seconds => {
-  // Your code here...
+  let countingTimer = setInterval(function () {
+    displayCountdown(convertTime(seconds));
+    seconds--;
+    if (seconds < 0) {
+      clearInterval(countingTimer);
+    }
+  }, 1000);
 };
+
+function convertTime (seconds) {
+  let minute= String(Math.floor(seconds / 60));
+  let second = String(seconds % 60);
+  if (minute.length === 1) {
+    minute ="0"+minute;
+  }
+  if (second.length === 1) {
+    second ="0"+second;
+  }
+  let time = `${minute}:${second}`
+  return time;
+}
+
 
 module.exports = {
   timer
