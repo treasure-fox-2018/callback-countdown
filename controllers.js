51412d1a6e86982a1cbcd31aa69bb2cd415d2acb
ronifajrian <ronifajrian@gmail.com>
2018-06-07 16:08:26 +0700

    release-0

--- controllers.js
+++ controllers.js
@@ -1,8 +1,16 @@
-const { displayCountdown } = require("./views");
+// const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  var counting = setInterval(function () {
+    console.log(seconds);
+    seconds--
+    if (seconds == 0) {
+      clearInterval(counting)
+    }
+  }, 1000)  
 };
+
 
 module.exports = {
   timer
