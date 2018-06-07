6d1e35bfae27d51962af49db1f12c2d98bcb3e26
Andre Sudi Yudatama <andresudi@DDCBU-elee.localdomain>
2018-06-07 17:27:12 +0700

    count down app done

--- controllers.js
+++ controllers.js
@@ -2,6 +2,14 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  var set = setInterval (function() {
+    if (seconds === 0){
+      clearInterval(set)
+    } else {
+      seconds--
+      displayCountdown(seconds)
+    }
+  }, 200)
 };
 
 module.exports = {
--- views.js
+++ views.js
@@ -4,8 +4,22 @@ const chalk = require("chalk");
 
 const displayCountdown = seconds => {
   clear();
-  console.log(chalk.cyan(figlet.textSync("Countdown App")));
-  // Your code here...
+
+  console.log(chalk.cyan(figlet.textSync('Count Down App')));
+  
+  let minute = Math.trunc(seconds/60)
+  let sec = seconds % 60
+
+  if (sec < 10) {
+    sec = '0' + sec
+  }
+
+  if(minute < 10) {
+    minute = '0'+ minute
+  }
+  
+  console.log(chalk.cyan(figlet.textSync(`${minute} : ${sec}`)));
+
 };
 
 module.exports = { displayCountdown };
