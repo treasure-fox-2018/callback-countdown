0a6f3905412222d9f22c2be9a35a9d164b86fc54
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2018-06-07 17:11:10 +0700

    callback-countdown Fadli Al Baihaqi done

--- controllers.js
+++ controllers.js
@@ -1,9 +1,28 @@
 const { displayCountdown } = require("./views");
 
-const timer = seconds => {
+const timer = (seconds) => {
   // Your code here...
+  let countingDown = setInterval(function() {
+    var minute = Math.floor(seconds/60)
+    var second = seconds % 60;
+  
+    if (minute < 10) {
+      minute = `0${minute}`;
+    }
+    if (second < 10) {
+      second = `0${second}`
+    }
+    displayCountdown (`${minute}:${second}`)
+    seconds--;
+    if (second === 0) {
+      clearInterval(countingDown)
+    }
+  }, 1000)
+  
+
 };
 
 module.exports = {
   timer
 };
+
---
+++ tes.js
@@ -0,0 +1,8 @@
+var detik = 100;
+var minute = detik/60;
+var rminute = Math.floor(minute)
+var second = (minute-rminute) * 60
+
+if (rminute < 10) {
+  console.log()
+}
