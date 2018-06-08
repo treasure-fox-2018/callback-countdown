0c434ac9b2b5a9385561957e46911fa4d6ed7d96
rizariza69 <riza.riza69@gmail.com>
2018-06-08 13:34:27 +0700

    riza

--- controllers.js
+++ controllers.js
@@ -1,9 +1,22 @@
 const { displayCountdown } = require("./views");
 
 const timer = seconds => {
-  // Your code here...
+  // Your code here..
+  let detik = 60 
+  let interval = setInterval(function(){
+    if (seconds > 0) {
+      seconds--
+      let minute = Math.ceil((second / detik)%10)
+      let second = Math.ceil((second % detik)/10)
+      let time = `${minute} : ${second}`
+      displayCountdown(time);
+    } else {
+      clearInterval(interval)
+    }
+  },1000);
+
 };
 
-module.exports = {
-  timer
-};
+module.exports = { timer };
+
+ 
