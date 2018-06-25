d93c6c578d1a9c7b07403bcc51989b0ecc999653
Tushar <tusharbedi7373@hotmail.com>
2018-06-25 16:58:28 +0700

    completed challenge countdown callback

--- controllers.js
+++ controllers.js
@@ -1,7 +1,13 @@
 const { displayCountdown } = require("./views");
 
 const timer = seconds => {
-  // Your code here...
+  let countdown = setInterval(function() {
+    seconds--
+    displayCountdown(seconds) 
+    if (seconds == 0) {
+      clearInterval(countdown)
+    }
+  }, 1000)
 };
 
 module.exports = {
--- views.js
+++ views.js
@@ -1,11 +1,21 @@
 const clear = require("clear");
 const figlet = require("figlet");
 const chalk = require("chalk");
+const controllers = require("./controllers.js")
 
 const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
-  // Your code here...
+  let MM = Math.floor(seconds/60)
+  let second = seconds%60
+
+  let SS = null;
+  if (second < 10) {
+    SS = `0${second}`
+  }
+  else SS = second; 
+  console.log(chalk.green.bold(figlet.textSync(`${MM} : ${SS}`)))
+
 };
 
 module.exports = { displayCountdown };
